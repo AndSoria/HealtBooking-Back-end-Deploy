897fef69f643a98fa0e0dbf373a311ff2d303a39
Andres Soria <andressoria08@gmail.com>
2023-12-15 02:43:15 -0300

    save change

--- api/src/controllers/loadDb/loadDbAppointment.js
+++ api/src/controllers/loadDb/loadDbAppointment.js
@@ -2,24 +2,23 @@
 const mockAppointment = require('../../assets/data/mockAppointment.json').appointments;
 const calcPrice = require('./calcPrice.js');
 const postAppointment = require('../postAppointment.js');
-const moment= require ('moment')
+const moment = require('moment');
+
 
 const loadDbAppointment = async () => {
   try {
     for (const appointment of mockAppointment) {
-
       const { date, time, idPatient, idDoctor, paymentDay, status } = appointment;
-
-
-      const dateMoment= await moment(new Date(date)).format( 'YYYY-MM-DD')
 
       const price = await calcPrice(idPatient, idDoctor);
 
-      await postAppointment( dateMoment, time, idPatient, idDoctor, price, paymentDay, status);
+      const dateMoment = await moment(new Date(date)).format('YYYY-MM-DD')
+
+      await postAppointment(dateMoment, time, idPatient, idDoctor, price, paymentDay, status);
 
     }
   } catch (error) {
-    console.log('Error loading appointments:', error);
+    console.error('Error loading appointments:', error);
   }
 };
 
